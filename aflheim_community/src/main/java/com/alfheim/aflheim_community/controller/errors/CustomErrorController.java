8040cf02ec300a2621e9f5632c32e399a648ccc9
Tomax47 <tammam744@gmail.com>
2024-04-30 17:15:05 +0300

    Added the 500 error page

--- aflheim_community/src/main/java/com/alfheim/aflheim_community/controller/errors/CustomErrorController.java
+++ aflheim_community/src/main/java/com/alfheim/aflheim_community/controller/errors/CustomErrorController.java
@@ -16,7 +16,12 @@ public class CustomErrorController implements ErrorController {
         Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (statusCode != null && Integer.valueOf(statusCode.toString()) == HttpStatus.NOT_FOUND.value()) {
+            // Not Found 404
             return "error_pages/not_found_page";
+
+        } else if (statusCode != null && Integer.valueOf(statusCode.toString()) == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            // Internal Server Error 500
+            return "error_pages/internal_server_error_page";
         }
 
         return "error_pages/internal_server_error_page";
